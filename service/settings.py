97d3530ba2c9906dc881f1e1c5a8e7da91fa204d
JMP <josemarfyl@gmail.com>
2019-03-01 17:28:48 +0100

    Add default secret key

--- service/settings.py
+++ service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'xd4r8j@yyu@w!j5t61njzkx&ver08ib3-swzs68ps0tq4m8qs5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ.get('DEBUG', 'True'))
