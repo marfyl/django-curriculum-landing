d4a73a0f98d40785614a4b7a53acc18e69cad136
JMP <josemarfyl@gmail.com>
2019-03-01 17:00:27 +0100

    Increase cache control max age

--- service/components/staticfiles.py
+++ service/components/staticfiles.py
@@ -3,7 +3,7 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-AWS_S3_ACTIVE = eval(os.environ.get('AWS_S3_ACTIVE', 'False'))
+AWS_S3_ACTIVE = eval(os.environ.get('AWS_S3_ACTIVE', 'True'))
 
 if AWS_S3_ACTIVE:
     # AWS
@@ -12,6 +12,9 @@ if AWS_S3_ACTIVE:
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')
     AWS_DEFAULT_ACL = None
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=864000',
+    }
 
     # STATIC
     STATICFILES_LOCATION = 'static'
